ea4a9a6c5d9cc9c05fc823273cc881200fc86ea7
Vanshika Goel <goelvanshika455@gmail.com>
2026-08-16 12:52:08 +0530

    Code till preprocessing

---
+++ ml/src/config.py
@@ -0,0 +1,23 @@
+FEATURES = [
+    "ch450",
+    "ch500",
+    "ch550",
+    "ch570",
+    "ch600",
+    "ch650"
+]
+
+MEDICINES = [
+    "paracetamol",
+    "aspirin",
+    "vitamin_c"
+]
+
+IDENTIFIER_COLUMNS = [
+    "sample_id",
+    "batch_id",
+    "manufacturer",
+    "measurement_id"
+]
+
+MODEL_DIR = "models"
